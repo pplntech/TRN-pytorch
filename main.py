56084839a8b349724b636b8a0eb57890a9f3bdd0
pplntech <kyungminkim@yonsei.ac.kr>
2018-08-24 10:57:06 +0900

    visualization

--- main.py
+++ main.py
@@ -248,7 +248,7 @@ def validate(val_loader, model, criterion, iter, log=None, json_file=None, idx2c
         if json_file is not None:
             output, attentions = model(input_var, eval=True)
         else:
-            output = model(input_var, eval=True)
+            output = model(input_var, eval=False)
         loss = criterion(output, target_var)
 
         # measure accuracy and record loss
