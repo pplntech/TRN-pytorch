9f6162f490f586f6d0cce4ad1b17986e65b7eba7
pplntech <kyungminkim@yonsei.ac.kr>
2018-09-27 14:53:49 +0900

    num seg 16

--- dataset.py
+++ dataset.py
@@ -87,16 +87,14 @@ class TSNDataSet(data.Dataset):
         """
 
         average_duration = (record.num_frames - self.new_length + 1) // self.num_segments
-        if record.num_frames < self.num_segments:
-            offsets = np.linspace(0,self.num_segments-1,self.num_segments,dtype='int64')
-        elif average_duration > 0:
+        if average_duration > 0:
             offsets = np.multiply(list(range(self.num_segments)), average_duration) + randint(average_duration, size=self.num_segments)
             # print (offsets.dtype)
         elif record.num_frames > self.num_segments:
             offsets = np.sort(randint(record.num_frames - self.new_length + 1, size=self.num_segments))
             # print (offsets.dtype)
         else:
-            offsets = np.linspace(0,self.num_segments-1,self.num_segments,dtype='int64')
+            offsets = np.linspace(0,self.num_frames-1,self.num_segments,dtype='int64')
             # print (offsets.dtype, record.path)
             # asdf
             # offsets = np.zeros((self.num_segments,), dtype=)
